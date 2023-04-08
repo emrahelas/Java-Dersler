bbbf0ad6090811a07649367701918a6dfe9cc2d5
emrahelas <ecivan@gmail.com>
2023-04-08 05:50:43 +0300

    ders42

--- src/ders39_maps/MapMethodDepo.java
+++ src/ders39_maps/MapMethodDepo.java
@@ -51,85 +51,172 @@ public class MapMethodDepo {
 
     }
 
-        public static Map<Integer, String> topluSubeDegistirme(Map < Integer, String > okulMap, String eskiSube, String
-        yeniSube){
+    public static Map<Integer, String> topluSubeDegistirme(Map<Integer, String> okulMap, String eskiSube, String
+            yeniSube) {
 
-            // 1- once key'leri bir Set olarak kaydedelim
-            Set<Integer> okulKeySeti = okulMap.keySet(); // [101, 102, 103, 104, 105, 106]
+        // 1- once key'leri bir Set olarak kaydedelim
+        Set<Integer> okulKeySeti = okulMap.keySet(); // [101, 102, 103, 104, 105, 106]
 
 
-            for (Integer eachKey : okulKeySeti
-            ) {
-                // 2- her bir key'e ait value'yu split ile array'e donusturelim
-                String[] valueArr = okulMap.get(eachKey).split("-"); // [Ali, Cem, 10, H, MF]
+        for (Integer eachKey : okulKeySeti
+        ) {
+            // 2- her bir key'e ait value'yu split ile array'e donusturelim
+            String[] valueArr = okulMap.get(eachKey).split("-"); // [Ali, Cem, 10, H, MF]
 
-                // 3- sube bilgisi eskiSube olanlari yeniSube yapalim
+            // 3- sube bilgisi eskiSube olanlari yeniSube yapalim
 
-                if (valueArr[3].equalsIgnoreCase(eskiSube)) {
-                    valueArr[3] = yeniSube; // [Ali, Cem, 10, H, MF]
-                }
-                // 4- array'deki bilgileri birlestirerek, yeniden map'e ekleyelim
-                // Ali-Cem-10-H-MF
+            if (valueArr[3].equalsIgnoreCase(eskiSube)) {
+                valueArr[3] = yeniSube; // [Ali, Cem, 10, H, MF]
+            }
+            // 4- array'deki bilgileri birlestirerek, yeniden map'e ekleyelim
+            // Ali-Cem-10-H-MF
 
-                String yeniValue = "";
-                for (int i = 0; i < valueArr.length - 1; i++) {
+            String yeniValue = "";
+            for (int i = 0; i < valueArr.length - 1; i++) {
 
-                    yeniValue += valueArr[i] + "-";
-                }
-
-                yeniValue += valueArr[valueArr.length - 1];
-
-                okulMap.put(eachKey, yeniValue);
-
+                yeniValue += valueArr[i] + "-";
             }
 
-            // 5- okulMap'inin son halini return edelim
+            yeniValue += valueArr[valueArr.length - 1];
 
-            return okulMap;
+            okulMap.put(eachKey, yeniValue);
 
         }
 
-        public static Map<Integer, String> yilSonuSiniflariDegistir (Map < Integer, String > okulMap){
+        // 5- okulMap'inin son halini return edelim
 
-            // 1- key'leri bir Set olarak kaydedelim
-            Set<Integer> keySeti = okulMap.keySet(); // // [101, 102, 103, 104, 105, 106]
-            // 2- Set'deki her bir key'e ait value'yu for-each loop ile ele alalim
-            for (Integer eachKey : keySeti
-            ) {
+        return okulMap;
 
-                // 3- her key'e ait value'yu split() ile ayirip, array olarak kaydedelim
-                String[] valueArr = okulMap.get(eachKey).split("-"); // [Ali, Cem, 10, H, MF]
-                // 4- array olarak kaydettigimiz bilgilerde istenen update'i yapalim
+    }
 
-                if (valueArr[2].equalsIgnoreCase("12")) {
-                    valueArr[2] = "Mezun";
-                } else { // "9"  "10"  "11"
 
-                    int sinif = Integer.parseInt(valueArr[2]);
-                    sinif++;
+    public static Map<Integer, String> yilSonuSiniflariDegistir(Map<Integer, String> okulMap) {
 
-                    valueArr[2] = sinif + "";
+        // 1- key'leri bir Set olarak kaydedelim
+        Set<Integer> keySeti = okulMap.keySet(); // // [101, 102, 103, 104, 105, 106]
+        // 2- Set'deki her bir key'e ait value'yu for-each loop ile ele alalim
+        for (Integer eachKey : keySeti
+        ) {
 
-                } // [Ali, Cem, 11, H, MF]
+            // 3- her key'e ait value'yu split() ile ayirip, array olarak kaydedelim
+            String[] valueArr = okulMap.get(eachKey).split("-"); // [Ali, Cem, 10, H, MF]
+            // 4- array olarak kaydettigimiz bilgilerde istenen update'i yapalim
 
-                // 5- array'in yeni halini String olarak birlestirip yeniValue elde edelim
+            if (valueArr[2].equalsIgnoreCase("12")) {
+                valueArr[2] = "Mezun";
+            } else { // "9"  "10"  "11"
 
-                String yeniValue = "";
+                int sinif = Integer.parseInt(valueArr[2]);
+                sinif++;
 
-                for (int i = 0; i < valueArr.length - 1; i++) {
+                valueArr[2] = sinif + "";
 
-                    yeniValue += valueArr[i] + "-";
-                }
+            } // [Ali, Cem, 11, H, MF]
 
-                yeniValue += valueArr[valueArr.length - 1];
+            // 5- array'in yeni halini String olarak birlestirip yeniValue elde edelim
 
-                // 6- her bir key ve yeniValue'yu map'e ekleyerek map'i update edelim
+            String yeniValue = "";
 
-                okulMap.put(eachKey, yeniValue);
+            for (int i = 0; i < valueArr.length - 1; i++) {
+
+                yeniValue += valueArr[i] + "-";
             }
 
-            // 7- map'in yeni halini return edelim
-            return okulMap;
+            yeniValue += valueArr[valueArr.length - 1];
+
+            // 6- her bir key ve yeniValue'yu map'e ekleyerek map'i update edelim
+
+            okulMap.put(eachKey, yeniValue);
         }
 
+        // 7- map'in yeni halini return edelim
+        return okulMap;
+    }
+
+    public static Map<Integer, String> numaraIleSoyisimDegistir(Map<Integer, String> okulMap, int okulNo, String yeniSoyisim) {
+
+        // 1- istenen value'yu alalim
+        String istenenOgrenciValue = okulMap.get(okulNo); // Veli-Can-10-M-TM
+
+        // 2- value uzerinde islem yapabilmek Split ile array'e kaydedelim
+
+        String[] valueArr = istenenOgrenciValue.split("-"); // [Veli, Can, 10, M, TM]
+
+        // 3- value array'i uzerinde istenen islemi yap
+
+        valueArr[1] = yeniSoyisim; // // [Veli, Han, 10, M, TM]
+
+        // 4- array'de degisiklik yapildiysa, map'i update etmemiz gerekir
+        //    bunun icin de array'i yeniden value yapisina uygun sekilde
+        //    String'e donusturmeliyiz
+
+
+        String yeniValue = valueArr[0] + "-" + valueArr[1] + "-" +
+                valueArr[2] + "-" + valueArr[3] + "-" + valueArr[4];
+
+        // 5- update edilmis yeni value'yu, key kullanarak map'e ekleyelim
+
+        okulMap.put(okulNo, yeniValue);
+
+        return okulMap;
+
+    }
+
+    public static void sinifaGoreListeYazdir(Map<Integer, String> okulMap, int istenenSinif) {
+
+        // 1- okulmap'deki entry'leri kaydedelim
+        Set<Map.Entry<Integer, String>> okulEntrySeti = okulMap.entrySet();
+
+        // 2- Her bir entry'i ele alip, istenen islemi yaptiralim
+        for (Map.Entry<Integer, String> eachEntry : okulEntrySeti
+        ) {
+
+            // eachEntry ==> 101=Ali-Cem-10-H-MF
+
+
+            // isim, soyisim, bolumlerini
+            String value = eachEntry.getValue(); // Ali-Cem-10-H-MF
+            String[] valueArr = value.split("-"); // [Veli, Can, 10, M, TM]
+
+            if (valueArr[2].equals(istenenSinif + "")) {
+                // no,
+                System.out.print(eachEntry.getKey() + " ");
+                // isim, soyisim, bolumlerini
+                System.out.print(valueArr[0] + " " + valueArr[1] + " " + valueArr[4]);
+                System.out.println("");
+            }
+
+        }
+
+
+    }
+
+    public static Map<Integer, String> soyisimleriBuyukYap(Map<Integer, String> okulMap) {
+
+        // Entry'ler ile cozelim
+        Set<Map.Entry<Integer, String>> okulEntrySeti = okulMap.entrySet();
+
+        // herbir Entry'i elden gecirip, sadece soyisimleri buyuk harf yapalim
+
+        for (Map.Entry<Integer, String> eachEntry : okulEntrySeti
+        ) {
+
+            // her bir entry'deki value'yu alalim
+            String value = eachEntry.getValue(); // Ali-Cem-10-H-MF
+
+            // value'yu array'e cevirelim
+            String[] valueArr = value.split("-"); // [Veli, Can, 10, M, TM]
+
+            // array icinde soyismi buyuk harf yapalim
+
+            valueArr[1] = valueArr[1].toUpperCase(); // [Veli, CAN, 10, M, TM]
+
+            eachEntry.setValue(valueArr[0] + "-" + valueArr[1] + "-" + valueArr[2] + "-" +
+                    valueArr[3] + "-" + valueArr[4] + "-");
+
+
+        }
+        return okulMap;
+
+    }
 }
